2e0e569656b67b75ff6214906e026d38a249958b
Thanh Tuan <thanhtuandang030397@gmail.com>
2021-03-01 17:56:55 +0700

    Middleware authenticate

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,20 +1,25 @@
 
 import { Route } from "@core/interfaces";
+import authMiddleware from "@core/middleware/auth.middleware";
 import { Router } from "express";
 import AuthController from "./auth.controller";
 
-export default class AuthRoute implements Route{
+export default class AuthRoute implements Route {
     public path = '/api/auth';
     public router = Router();
 
     public userController = new AuthController();
 
-    constructor(){
+    constructor() {
         this.initializeRoutes();
     }
 
-    private initializeRoutes(){
+    private initializeRoutes() {
         this.router.post(this.path,
-        this.userController.login); // post: http://localhost:5000/api/auth
+            this.userController.login); // post: http://localhost:5000/api/auth
+
+        this.router.get(this.path,
+            authMiddleware,
+            this.userController.getCurrentLoginUser); // GET: http://localhost:5000/api/auth -> required login
     }
 }
