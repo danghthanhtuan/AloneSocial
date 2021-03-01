d5a61dac340fffa291826fd5e5d5885625a2e41d
Thanh Tuan <thanhtuandang030397@gmail.com>
2021-03-01 17:57:14 +0700

    Add middleware autheticate

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,17 +3,27 @@ import { NextFunction, Request, Response } from "express";
 import LoginDto from "./auth.dto";
 import AuthService from "./auth.service";
 
-export default class AuthController{
+export default class AuthController {
 
-    private userService = new AuthService();
+    private authService = new AuthService();
 
-    public login = async (req: Request, res: Response, next: NextFunction)=>{
-        try{
+    public login = async (req: Request, res: Response, next: NextFunction) => {
+        try {
             const model: LoginDto = req.body;
-            const tokenData: TokenData = await this.userService.login(model);
+            const tokenData: TokenData = await this.authService.login(model);
 
             res.status(200).json(tokenData);
-        }catch(error){
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public getCurrentLoginUser = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const user = await this.authService.getCurrentLoginUser(req.user.id);
+
+            res.status(200).json(user);
+        } catch (error) {
             next(error);
         }
     };
