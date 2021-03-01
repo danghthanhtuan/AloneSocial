d24b88eab3c00ed524f8b0bbc3380e715b580358
Thanh Tuan <thanhtuandang030397@gmail.com>
2021-03-01 16:39:01 +0700

    add login

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import 'dotenv/config'
 import { IndexRoute } from '@modules/index'
 import App from './app'
 import UserRoute from '@modules/users/user.route';
+import AuthRoute from '@modules/auth/auth.route';
 
 const routes = [
     new IndexRoute(),
-    new UserRoute()
+    new UserRoute(),
+    new AuthRoute()
 ];
 const app = new App(routes);
 
